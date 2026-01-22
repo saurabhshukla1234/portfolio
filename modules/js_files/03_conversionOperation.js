558554f668b2cd3deeba248af07a34f120248da9
saurabhshukla1234 <saurabhkumarshukla4075@gmail.com>
2026-01-22 14:06:15 +0530

    operation is add

--- modules/js_files/03_conversionOperation.js
+++ modules/js_files/03_conversionOperation.js
@@ -7,8 +7,8 @@ console.log(typeof score);
 
 let valueinNumber = Number(score)
 
-console.log(typeof(valueinNumber))
-console.log(valueinNumber)
+console.log(typeof(valueinNumber));
+console.log(valueinNumber);
 
 //for string to number value
 
@@ -19,8 +19,8 @@ console.log(num);
                                                                                         // string --> NaN
 let numberValue = Number(num)
 
-console.log(typeof(numberValue))
-console.log(numberValue)
+console.log(typeof(numberValue));
+console.log(numberValue);
 
 
 //for number to string conversion
@@ -28,8 +28,8 @@ console.log(numberValue)
 let numTostring = 33
 
 let numInstring = String(numTostring)
-console.log(numInstring)
-console.log(typeof numInstring)
+console.log(numInstring);
+console.log(typeof numInstring);
 
 
 // for null value 
@@ -41,8 +41,8 @@ console.log(value1);
 
 let ValueNum = Number(value1)                                                    // null gives value -->0
 
-console.log(typeof(ValueNum))
-console.log(ValueNum)
+console.log(typeof(ValueNum));
+console.log(ValueNum);
 
 
 //for undefined
@@ -54,8 +54,8 @@ console.log(value2);
 
 let numValue = Number(value2)                    //undefined --> NaN
 
-console.log(typeof(numValue))
-console.log(numValue)
+console.log(typeof(numValue));
+console.log(numValue);
 
 
 //for boolean conversion
@@ -67,6 +67,56 @@ console.log(typeof TruFal);                                                  // 
                                                                              //   0 --> false; false --> 0
 let boolenNum = Boolean(TruFal)                                              //  1 --> true; true --> 1
 
-console.log(typeof(boolenNum))
-console.log(boolenNum)
-                                                           
+console.log(typeof(boolenNum));
+console.log(boolenNum);
+
+
+
+
+
+
+//***************************************************Operation*************************************************************** */
+
+
+
+console.log(2+2);       //4
+console.log(2-2);       //0
+console.log(2*2);       //4
+console.log(2/2);       //1
+console.log(2**3);      //8
+console.log(2%3);       //2
+
+let someNumber = 3
+let numberInnegative = -someNumber
+console.log(numberInnegative);
+
+let str1 = "hello"
+let str2 = " saurabh"
+let str3 = str1 + str2
+
+console.log(str3);              //hello saurabh
+
+console.log(2 + "1");           //21
+console.log("1" + 2);           //12
+console.log("1" + 2 + 2);       //122
+console.log(1 + 2 +"2");        //32
+
+// console.log( (3 + 7) * 5 % 8);       good method to write code
+
+console.log(+true);         //1
+console.log(+"");           //0
+
+
+
+let num1 , num2 , num3;
+num1 = num2 = num3 = 2 + 2;
+
+
+
+let gameCounter = 100
+gameCounter++;                                      //postfix increment
+console.log(gameCounter);               //101
+++gameCounter;                                      //prefix increment
+console.log(gameCounter);               //101
+
+
