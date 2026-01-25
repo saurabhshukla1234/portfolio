ee5b4a1b375e11480cf8e46679aeb3e5c5004cb8
saurabhshukla1234 <saurabhkumarshukla4075@gmail.com>
2026-01-25 17:01:40 +0530

    new files

---
+++ modules/js_files/05_strings.js
@@ -0,0 +1,48 @@
+let name = "saurabh "
+let repoCount = 50
+
+console.log(name + repoCount + " value"); // it's outdated 
+
+
+console.log(`hello my name is ${name} and my repocount is ${repoCount}`);
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const gameName = new String('saurabhsh-shukla-com')
+
+console.log(gameName[0]);
+
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+
+console.log(gameName.indexOf('s'));
+
+console.log(gameName.substring(0,4));
+
+console.log(gameName.slice(-8,4));
+
+console.log(gameName.split('-'));
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let newstring = "   xxxxxxxx    "
+
+console.log(newstring);
+
+console.log(newstring.trim());
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let url = "https://saurabhsukla.com/saurabh%20shukla"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('1'));
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
